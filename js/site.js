4ced3b79b0e3baa2f2457e5a3bc7d0207efc9c7e
miladnnouri <94610869+miladnnouri@users.noreply.github.com>
2024-10-23 11:45:43 +0330

    nataghi-proj-cimmit

---
+++ js/site.js
@@ -0,0 +1,19 @@
+﻿function remove(){
+    var selectElement = document.getElementById("MembershipType").value;
+    var selectMembership1 = document.getElementById("selectMembership1");
+    var selectMembership2 = document.getElementById("selectMembership2");
+
+    if (selectElement == "1") {
+        selectMembership1.classList.remove("hidden");
+        selectMembership2.classList.add("hidden");
+    }
+    if (selectElement == "2") {
+        selectMembership2.classList.remove("hidden");
+        selectMembership1.classList.add("hidden");
+    }
+    if (selectElement == "0") {
+      selectMembership2.classList.add("hidden");
+      selectMembership1.classList.add("hidden");
+    }
+
+}
